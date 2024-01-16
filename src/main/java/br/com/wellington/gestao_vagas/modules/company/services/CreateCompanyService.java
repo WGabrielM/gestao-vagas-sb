7a43c70492c1586bb88e1b93ce07197bd4bb6f27
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-01-16 08:50:38 -0300

    implements company service

--- src/main/java/br/com/wellington/gestao_vagas/modules/company/services/CreateCompanyService.java
+++ src/main/java/br/com/wellington/gestao_vagas/modules/company/services/CreateCompanyService.java
@@ -1,4 +1,25 @@
 package br.com.wellington.gestao_vagas.modules.company.services;
 
+import br.com.wellington.gestao_vagas.exceptions.UserFoundException;
+import br.com.wellington.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.wellington.gestao_vagas.modules.company.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CreateCompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+
+    public CompanyEntity execute(CompanyEntity companyEntity) {
+
+        this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
+            .ifPresent((user) -> {
+                throw new UserFoundException();
+            });
+
+        return this.companyRepository.save(companyEntity);
+    }
 }
