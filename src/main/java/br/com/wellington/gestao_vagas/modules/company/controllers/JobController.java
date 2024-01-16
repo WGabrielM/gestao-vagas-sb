4a4b18947d0e265439764e9fe50d5f4c376734bc
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-01-16 19:35:34 -0300

    job controller final ajustments

---
+++ src/main/java/br/com/wellington/gestao_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,21 @@
+package br.com.wellington.gestao_vagas.modules.company.controllers;
+
+import br.com.wellington.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.wellington.gestao_vagas.modules.company.services.CreateJobService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+    @Autowired
+    private CreateJobService createJobService;
+    @PostMapping("/")
+    public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+        return this.createJobService.execute(jobEntity);
+    }
+}
--- src/main/java/br/com/wellington/gestao_vagas/modules/company/services/CreateJobService.java
+++ src/main/java/br/com/wellington/gestao_vagas/modules/company/services/CreateJobService.java
@@ -1,11 +1,16 @@
 package br.com.wellington.gestao_vagas.modules.company.services;
 
 import br.com.wellington.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.wellington.gestao_vagas.modules.company.repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateJobService {
-    public void execute(JobEntity jobEntity) {
 
+    @Autowired
+    private JobRepository jobRepository;
+    public JobEntity execute(JobEntity jobEntity) {
+        return this.jobRepository.save(jobEntity);
     }
 }
