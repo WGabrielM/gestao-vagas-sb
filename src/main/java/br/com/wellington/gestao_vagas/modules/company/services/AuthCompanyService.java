0db70e3c6599e23431ddadfb51744b67d701f70a
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-01-30 18:00:17 -0300

    create AuthCompanyController

--- src/main/java/br/com/wellington/gestao_vagas/modules/company/services/AuthCompanyService.java
+++ src/main/java/br/com/wellington/gestao_vagas/modules/company/services/AuthCompanyService.java
@@ -2,7 +2,10 @@ package br.com.wellington.gestao_vagas.modules.company.services;
 
 import br.com.wellington.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.wellington.gestao_vagas.modules.company.repositories.CompanyRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,15 +14,17 @@ import javax.naming.AuthenticationException;
 
 @Service
 public class AuthCompanyService {
+    @Value("${security.token.secret}")
+    private String secretKey;
     @Autowired
     private CompanyRepository companyRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> {
-                    throw new UsernameNotFoundException("Company not found");
+                    throw new UsernameNotFoundException("Username/password incorrect");
                 }
         );
         var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
@@ -27,6 +32,11 @@ public class AuthCompanyService {
         if (!passwordMatches) {
             throw new AuthenticationException();
         }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javagas").withSubject(company.getId().toString()).sign(algorithm);
+        return token;
+
 
     }
 }
